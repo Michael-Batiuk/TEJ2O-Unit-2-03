8f7cfcd1b0f21fed258c329aea1c072fc9d15c6c
Michael-Batiuk <Michael.Batiuk@stu.ocsb.ca>
2025-09-16 13:26:09 -0400

    Does Math

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael Batiuk 
+ * Created on: Sep 2025
+ * This program does basic math.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A rectangle has dimensions 5 cm & 3 cm')
+basic.pause(100)
+basic.clearScreen()
+
+// Perimerter
+basic.showString('The perimeter would be:' + (5 + 5 + 3 + 3).toString() + 'cm')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
+
+// Area
+basic.showString('The area would be:' + (5 * 3).toString() + 'cm^2')
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.clearScreen()
